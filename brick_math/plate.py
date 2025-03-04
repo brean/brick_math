accb61491f2b0782a11f04f5a2c92edb0d4ce226
Andreas Bresser <self@andreasbresser.de>
2025-03-04 22:27:57 +0100

    move stud positions to center

--- brick_math/plate.py
+++ brick_math/plate.py
@@ -3,5 +3,5 @@ from .part import Part
 
 DEFAULT_PLATE_32x32 = Part(
     width=32*20, depth=32*20, connectors=[
-        [x, 1, z] for x in range(32) for z in range(32)
+        [x+10, 1, z+10] for x in range(32) for z in range(32)
     ])
